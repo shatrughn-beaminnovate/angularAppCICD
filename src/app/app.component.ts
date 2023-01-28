e284470b8ab23e8964a2bc5e46b88af1aeb17c2c
Shatrughn <68953592+shatrughn-beaminnovate@users.noreply.github.com>
2023-01-28 18:49:07 +0530

    Project Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'AngularDeployTest';
 
   constructor() {
 
   }
 
-  ngOnInit(): void {
-    // console.log('Id : ', this.name);
-  }
 }
